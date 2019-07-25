9459f765fd2d01c059a034066eb978a879ec082c
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-25 03:24:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,27 +8,21 @@ def map(source_array)
     new_array 
 end
 
-# def reduce(source_array, starting_value = 0)
-#   i = 0
-#   while i < source_array.length
-#     yield(source_array[i], starting_value)
-#     starting_value += source_array[i]
-#     i += 1
-#   end
-#   starting_value
-# end
-
-def reduce(source_array, starting_value=0)
+def reduce(source_array, starting_value = nil)
   i = 0
-  while i < source_array.length
-    if source_array[i]
-      yield(source_array[i], source_array[(i+1)%source_array.length])
-      return true
-    end
-    i+=1
+  if starting_value == nil
+    starting_value = source_array.first
+    i += 1
   end
-  false
+  
+  while i < source_array.length do
+    starting_value = yield(starting_value, source_array[i])
+    i += 1
+  end
+  starting_value
 end
+
+
 
 
 
